b5597eb8301b434adebb531dd59eb5e65ddfd9db
Veronika Martinovich <veranika.mart@gmail.com>
2020-08-07 22:12:09 +0300

    feat: add error and success sounds on bird click

--- src/components/BirdsList/BirdsList.js
+++ src/components/BirdsList/BirdsList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import "./BirdsList.scss";
 import { appStateSelector } from "../../reducers/app/appReducer";
@@ -8,6 +8,8 @@ import {
   actionSetErrorGameResult,
   actionSetCurrentBird,
 } from "../../reducers/app/appActions";
+import errorSound from "../../assets/error_sound.mp3";
+import successSound from "../../assets/success_sound.mp3";
 
 export const BirdsList = () => {
   const {
@@ -19,35 +21,46 @@ export const BirdsList = () => {
   } = useSelector(appStateSelector);
   const { birdsData } = useSelector(birdsSelector);
   const birds = birdsData[currentCategory];
+  const audioElement = useRef(null);
   const dispatch = useDispatch();
 
   const handleBirdItemClick = (e) => {
     const curBirdId = Number(e.currentTarget.dataset.birdId);
     const curBird = birds.find((item) => item.id === curBirdId);
     dispatch(actionSetCurrentBird(curBird));
-    
+
     if (curBirdId === randomBird.id && !isAnswerCorrect) {
       dispatch(actionSetSuccessGameResult(totalScore + currentPoints));
       e.currentTarget.firstChild.classList.add("answer-indicator_success");
+      document.querySelector(".audio_result").setAttribute("src", successSound);
     } else if (curBirdId !== randomBird.id && !isAnswerCorrect) {
       dispatch(actionSetErrorGameResult(currentPoints - 1));
       e.currentTarget.firstChild.classList.add("answer-indicator_error");
+      audioElement.current.setAttribute("src", errorSound);
     }
   };
 
   return (
-    <ul className="birds-list">
-      {birds.map((item) => (
-        <li
-          key={item.id}
-          data-bird-id={item.id}
-          className="birds-list__item"
-          onClick={handleBirdItemClick}
-        >
-          <span className="answer-indicator"></span>
-          {item.name}
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul className="birds-list">
+        {birds.map((item) => (
+          <li
+            key={item.id}
+            data-bird-id={item.id}
+            className="birds-list__item"
+            onClick={handleBirdItemClick}
+          >
+            <span className="answer-indicator"></span>
+            {item.name}
+          </li>
+        ))}
+      </ul>
+      <audio
+        ref={audioElement}
+        autoPlay
+        className="audio audio_result"
+        src=""
+      ></audio>
+    </>
   );
 };
